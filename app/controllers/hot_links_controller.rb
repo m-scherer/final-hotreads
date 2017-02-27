11b8adbb008c8d5d2545c89edf0abe7dfba0ba58
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-27 16:23:45 -0700

    Add api endpoint to retrieve hotreads

--- app/controllers/hot_links_controller.rb
+++ app/controllers/hot_links_controller.rb
@@ -1,7 +1,7 @@
 class HotLinksController < ApplicationController
 
   def index
-    @hot_links = HotLink.select('hot_links.url, count(hot_links.url) as url_count').group('hot_links.url').order('url_count desc').limit(10)
+    @hot_links = hot_reads
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :hot_links, only: [:create]
+      resources :hot_links, only: [:create, :index]
     end
   end
 end
