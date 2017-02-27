92c5b8637dbd0ad346b1e70f458ba9a9e45a27ff
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-27 15:45:46 -0700

    Refactor ActiveRecord query to meet specs

--- app/controllers/hot_links_controller.rb
+++ app/controllers/hot_links_controller.rb
@@ -1,7 +1,7 @@
 class HotLinksController < ApplicationController
 
   def index
-    @hot_links = HotLink.all
+    @hot_links = HotLink.select('hot_links.url, count(hot_links.url) as url_count').group('hot_links.url').order('url_count desc').limit(10)
   end
 
 end
